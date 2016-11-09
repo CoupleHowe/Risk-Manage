1b5f128b7a4ab0eb7870feaff45d446e2a69a4c3
QQ <839889341@qq.com>
2016-11-10 01:26:36 +0800

    flush

--- src/main/java/servlets/AddUser.java
+++ src/main/java/servlets/AddUser.java
@@ -1,4 +1,5 @@
 package servlets;
+
 import java.io.IOException;
 import java.io.PrintWriter;
 
@@ -9,54 +10,58 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import proxy.UserDaoProxy;
-import vo.UserVo;  
+import vo.UserVo;
 
+@WebServlet("/addUser")
+public class AddUser extends HttpServlet {
+	private static final long serialVersionUID = 1L;
 
-@WebServlet("/addUser")  
-public class AddUser extends HttpServlet {  
-    private static final long serialVersionUID = 1L; 
-    
-    /** 
-     * @see HttpServlet#HttpServlet() 
-     */  
-    public AddUser() {  
-        super();  
-    }  
-  
-    /** 
-     * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response) 
-     */  
-    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        response.setContentType("text/html;charset=UTF-8");  
-        PrintWriter out = response.getWriter();
+	/**
+	 * @see HttpServlet#HttpServlet()
+	 */
+	public AddUser() {
+		super();
+	}
 
-        try {  
-            response.setContentType("text/html");  
-            response.setHeader("Cache-Control", "no-store");  
-            response.setHeader("Pragma", "no-cache");  
-            response.setDateHeader("Expires", 0); 
-            
-	    	String name = request.getParameter("user_name");  
-	    	String password=request.getParameter("password"); 
-	    	UserVo userVo = new UserVo(name, password);
-	    	UserDaoProxy userDaoProxy = new UserDaoProxy();
-	    	
-	    	if(userDaoProxy.register(userVo))
-	    		out.write("YES");
-	    	else 
-	    		out.write("NO");
-	    	
-        } catch (Exception e) {
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		response.setContentType("text/html;charset=UTF-8");
+		PrintWriter out = response.getWriter();
+
+		try {
+			response.setContentType("text/html");
+			response.setHeader("Cache-Control", "no-store");
+			response.setHeader("Pragma", "no-cache");
+			response.setDateHeader("Expires", 0);
+
+			String name = request.getParameter("user_name");
+			String password = request.getParameter("password");
+			UserVo userVo = new UserVo(name, password);
+			UserDaoProxy userDaoProxy = new UserDaoProxy();
+
+			if (userDaoProxy.register(userVo))
+				out.write("YES");
+			else
+				out.write("NO");
+
+			out.flush();
+
+		} catch (Exception e) {
 			e.printStackTrace();
-		} finally {   
-            out.close();  
-        }   
-    }  
-  
-    /** 
-     * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response) 
-     */  
-    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {  
-    	doGet(request, response);  
-    }  
-}  
+		} finally {
+		}
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		doGet(request, response);
+	}
+}
--- src/main/java/servlets/FindUser.java
+++ src/main/java/servlets/FindUser.java
@@ -1,4 +1,5 @@
 package servlets;
+
 import java.io.IOException;
 import java.io.PrintWriter;
 
@@ -9,57 +10,62 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import proxy.UserDaoProxy;
-import vo.UserVo;  
+import vo.UserVo;
 
-@WebServlet("/findUser")  
-public class FindUser extends HttpServlet {  
-    private static final long serialVersionUID = 1L; 
-    
-    /** 
-     * @see HttpServlet#HttpServlet() 
-     */  
-    public FindUser() {  
-        super();  
-    }  
-  
-    /** 
-     * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response) 
-     */  
-    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        response.setContentType("text/html;charset=UTF-8");  
-        PrintWriter out = response.getWriter();
-        
-        try {  
-            response.setContentType("text/html");  
-            response.setHeader("Cache-Control", "no-store");  
-            response.setHeader("Pragma", "no-cache");  
-            response.setDateHeader("Expires", 0); 
-            
-	    	String name=request.getParameter("user_name");  
-	    	String password=request.getParameter("password"); 
-	    	UserVo userVo = new UserVo(name, password);
-	    	UserDaoProxy userDaoProxy = new UserDaoProxy();
-	    	
-	    	if(userDaoProxy.findLogin(userVo))
-	    		out.write("YES");
-	    	else 
-	    		out.write("NO");
-	    	
-        } catch (Exception e) {
+@WebServlet("/findUser")
+public class FindUser extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * @see HttpServlet#HttpServlet()
+	 */
+	public FindUser() {
+		super();
+	}
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		response.setContentType("text/html;charset=UTF-8");
+		PrintWriter out = response.getWriter();
+
+		try {
+			response.setContentType("text/html");
+			response.setHeader("Cache-Control", "no-store");
+			response.setHeader("Pragma", "no-cache");
+			response.setDateHeader("Expires", 0);
+
+			String name = request.getParameter("user_name");
+			String password = request.getParameter("password");
+			UserVo userVo = new UserVo(name, password);
+			UserDaoProxy userDaoProxy = new UserDaoProxy();
+
+			if (userDaoProxy.findLogin(userVo))
+				out.write("YES");
+			else
+				out.write("NO");
+
+			out.flush();
+
+		} catch (Exception e) {
 			e.printStackTrace();
-		} finally {   
-            out.close();  
-        }  
-        
-		//	    request.setAttribute("info", info); 
-		//	    request.getRequestDispatcher("Login.jsp").forward(request,response); 
-      
-    }  
-  
-    /** 
-     * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response) 
-     */  
-    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {  
-      doGet(request, response);  
-    }  
+		} finally {
+		}
+
+		// request.setAttribute("info", info);
+		// request.getRequestDispatcher("Login.jsp").forward(request,response);
+
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		doGet(request, response);
+	}
 }
