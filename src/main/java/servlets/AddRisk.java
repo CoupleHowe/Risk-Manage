4371c67d4a5a679c85fe8036f9d74a6f9ef8bdd4
QQ <839889341@qq.com>
2016-11-10 13:11:39 +0800

    add risk jsp

--- src/main/java/servlets/AddRisk.java
+++ src/main/java/servlets/AddRisk.java
@@ -1,5 +1,67 @@
 package servlets;
 
-public class AddRisk {
+import java.io.IOException;
+import java.io.PrintWriter;
 
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import proxy.UserDaoProxy;
+import vo.UserVo;
+
+@WebServlet("/addRisk")
+public class AddRisk extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * @see HttpServlet#HttpServlet()
+	 */
+	public AddRisk() {
+		super();
+	}
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		response.setContentType("text/html;charset=UTF-8");
+		PrintWriter out = response.getWriter();
+
+		try {
+			response.setContentType("text/html");
+			response.setHeader("Cache-Control", "no-store");
+			response.setHeader("Pragma", "no-cache");
+			response.setDateHeader("Expires", 0);
+
+			String name = request.getParameter("user_name");
+			String password = request.getParameter("password");
+			UserVo userVo = new UserVo(name, password);
+			UserDaoProxy userDaoProxy = new UserDaoProxy();
+
+			if (userDaoProxy.register(userVo))
+				out.write("YES");
+			else
+				out.write("NO");
+
+			out.flush();
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+		}
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		doGet(request, response);
+	}
 }
